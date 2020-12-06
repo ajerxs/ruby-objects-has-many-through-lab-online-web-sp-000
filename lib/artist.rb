99137e1450a7e520b9b97dafbd5a643033cf0e3c
Andrew Eames <ajerxs@gmail.com>
2020-12-06 20:09:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,19 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song, :genre
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @song = song
+    @genre = genre
     @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def new_song()
 
 end
