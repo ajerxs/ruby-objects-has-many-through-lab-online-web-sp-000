9d7a04e468eb6a35a820b679c5ac670c402b26c1
Andrew Eames <ajerxs@gmail.com>
2020-12-06 20:56:49 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,15 @@
+class patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+end
